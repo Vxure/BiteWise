a1c1de9a9fba824206fd6b425c5e08a3c5c590f6
Regan <regan_li@sfu.ca>
2025-06-16 16:33:00 -0700

    Initial Commit

---
+++ BiteWise/BiteWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiteWiseApp.swift
+//  BiteWise
+//
+//  Created by Regan on 2025-06-16.
+//
+
+import SwiftUI
+
+@main
+struct BiteWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiteWiseTests/BiteWiseTests.swift
@@ -0,0 +1,17 @@
+//
+//  BiteWiseTests.swift
+//  BiteWiseTests
+//
+//  Created by Regan on 2025-06-16.
+//
+
+import Testing
+@testable import BiteWise
+
+struct BiteWiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
